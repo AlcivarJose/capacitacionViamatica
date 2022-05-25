1b6c32fa41f49cfbe1424b9ce61f338542eeac86
AlcivarJose <jose.alcivarvi@ug.edu.ec>
2022-05-25 14:32:49 -0500

    ultimos cambios 25-5-2022

---
+++ src/app/contador.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-contador',
+    template: `
+        <h1>Victoria mi vida </h1>
+        <button (click)="acumular(-base)">-1</button>
+        <span>{{numero}}</span>
+        <button (click)="acumular(base);">+1</button> 
+    `           
+  })
+
+export class ContadorComponent{
+    title = 'Capacitacion Viamatica';
+    numero:number =  10;
+    base  :number = 5;
+  
+    acumular (valor:number){
+      this.numero+=valor; 
+    }
+
+}
